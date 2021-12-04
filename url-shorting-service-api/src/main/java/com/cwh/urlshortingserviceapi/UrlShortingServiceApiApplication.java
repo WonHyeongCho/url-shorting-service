dec5ef6633d901ad5990ba2a3503d5f124d521ab
WonhyeongCho <tmzkdls7989@gmail.com>
2021-12-04 19:36:40 +0900

    feat: project init
    Signed-off-by: WonhyeongCho <tmzkdls7989@gmail.com>

---
+++ url-shorting-service-api/src/main/java/com/cwh/urlshortingserviceapi/UrlShortingServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.cwh.urlshortingserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortingServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortingServiceApiApplication.class, args);
+	}
+
+}
---
+++ url-shorting-service-api/src/test/java/com/cwh/urlshortingserviceapi/UrlShortingServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwh.urlshortingserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortingServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
